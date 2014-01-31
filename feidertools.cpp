e32731cfe6a41404504fc05f8ad4f75154056a94
Felix Schneider <feider@mint>
2014-01-31 12:40:04 +0100

    began with the sorted vector

--- feidertools.cpp
+++ feidertools.cpp
@@ -39,7 +39,70 @@ namespace fdr
 			return_vector.push_back(std::string(token));
 		}
 		while(rest.compare("") != 0);
-		
+
 		return return_vector;
 	}
+
+
+	void Storeable::sort_by(unsigned int * sort_by)
+	{
+		this->sort_val = sort_by;
+	}
+
+	unsigned int Storeable::get_sort_val()
+	{
+		return *(this->sort_val);
+	}
+
+
+	void Sorted_Vector::insert_from_back(Storeable * storeable)
+	{
+		if(this->intern_vector.size() == 0)
+		{
+			this->intern_vector.push_back(storeable);
+			this->move = false;
+			this->middle_element = this->intern_vector.begin();
+		}
+		else
+		{
+			this->insert(storeable, this->intern_vector.end());
+		}
+	}
+
+	void Sorted_Vector::insert_from_front(Storeable * storeable)
+	{
+		if(this->intern_vector.size() == 0)
+		{
+			this->intern_vector.push_back(storeable);
+			this->move = false;
+			this->middle_element = this->intern_vector.begin();
+		}
+		else
+		{
+			this->insert(storeable, this->intern_vector.begin());
+		}
+	}
+
+
+
+	void Sorted_Vector::insert_from_middle(Storeable * storeable)
+	{
+		if(this->intern_vector.size() == 0)
+		{
+			this->intern_vector.push_back(storeable);
+			this->move = false;
+			this->middle_element = this->intern_vector.begin();
+		}
+		else
+		{
+			this->insert(storeable, this->intern_vector.begin());
+		}
+	}
+
+	void Sorted_Vector::insert(Storeable * storeable, std::vector<Storeable*>::iterator position)
+	{
+		// do nothing
+	}
+
+
 }
--- feidertools.hpp
+++ feidertools.hpp
@@ -12,4 +12,50 @@ namespace fdr
 
 	std::vector<std::string> tokenise(const std::string & str, const std::string & delim);
 
+
+
+
+
+
+
+	/*
+	 * Objects that can be stored in the sorted vector
+	 * should be derived from this class
+	 * the sort_val must be set for the sorted vector
+	 * to sort by
+	 */
+
+	class Storeable
+	{
+	private:
+		unsigned int * sort_val;
+	public:
+		void sort_by(unsigned int * sort_by);
+		unsigned int get_sort_val();
+	};
+
+	/* This data structure stores a sorted vector
+	 * new elements are automatically inserted
+	 * sorted by storeable->get_sort_val()
+	 * The user can determine from where the new element is inserted
+	 * depending on the application it could be useful
+	 * to search from the beginning, the end or the middle
+	 */
+
+	class Sorted_Vector
+	{
+	protected:
+		std::vector<Storeable*> intern_vector;
+		std::vector<Storeable*>::iterator middle_element; //this points to the middle element of the vector. It must be moved by the insert functions according to the bool move
+		bool move; // if true, middle_element is about to be moved right. If false, middle_element is about to be moved left
+	public:
+		std::vector<Storeable*> & vector();
+
+		void insert_from_back(Storeable * storeable);
+		void insert_from_front(Storeable * storeable);
+		void insert_from_middle(Storeable * storeable);
+		void insert(Storeable * storeable, std::vector<Storeable*>::iterator position);
+
+	};
+
 }
